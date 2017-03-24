1ed9ed1d73a50c5f7fc81da8d0368de9f0fa283c
diazg77 <gerojd@yahoo.com>
2017-03-24 09:59:55 -0500

    new files

---
+++ XenexDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XenexDemoPrint;
+
+namespace XenexDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var messageCases = new Dictionary<int, string>();
+            messageCases.Add(3, "fizz");
+            messageCases.Add(5, "buzz");
+            foreach (string message in XenexPrint.GenerateMessages(20, messageCases))
+                Console.WriteLine(message);
+
+            Console.ReadLine();
+        }
+    }
+}
